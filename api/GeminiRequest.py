22fa7d66837ab471576006d547a2e8be73994cbd
Coby Durling <Coby Durling>
2017-05-06 00:15:43 -0400

    Working API requests.  Moved to single file

--- api/GeminiRequest.py
+++ api/GeminiRequest.py
@@ -1,92 +1,66 @@
 import requests
+import time
+import configparser
+import base64
+import hmac
+import hashlib
+import json
 import logging as log
 
 class GeminiRequest(object):
     """
-    Class that makes calls to the Gemini API.
-
-    methods:
-    
-    __init__..............................initialize the current request.  Uses the 
-                                          sandbox url, and 'GET' by default for url
-                                          and request method.  Other features are 
-                                          left blank.
-    call..................................execute the current request
-    update_headers........................update the headers of the current request
+    Client that will make requests to Gemini
     """
 
-    def __init__(self, request_type = None, base_url = None, path = None, port = None, 
-                 headers = None, auth = None, verify = None, key = None, data = None):
-
-        if request_type is None:
-            self.request_type = 'GET'
-        else:
-            self.request_type = request_type
-
-        if base_url is None:
-            self.base_url = 'https://api.sandbox.gemini.com'
-            log.info('URL Not set.  Using %s by default'%self.base_url)
-        else:
-            self.base_url = base_url
-
-        if path is None:
-            self.path = ''
-            log.warning('Path not set.  Request will most likely fail.')
-        else:
-            self.path = path
-
-        if port is None:
-            self.port = ''
-        else:
-            self.port = port
-
-        if headers is None:
-            self.headers = {}
-        else:
-            self.headers = headers
-
-        if auth is None:
-            self.auth = ()
-            log.warning('Auth not set.  Request will most likely fail.')
-        else:
-            self.auth = auth
-
-        if verify is None:
-            self.verify = False
-        else:
-            self.verify = verify
-
-        if key is None:
-            self.key = ''
-            log.warning('No API key given.  Request will most likely fail.')
-        else:
-            self.key = key
-
-        if data is None:
-            self.data = {}
-        else:
-            self.data = data
-
-        self.url = '%(url)s/%(path)s' % {'url':self.base_url, 'path':self.path}
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('../settings.config')
+        self.url = config['API_SETTINGS']['url']
+        self.api_key = config['API_SETTINGS']['api_key']
+        self.api_secret = config['API_SETTINGS']['api_secret']
 
 
-    def call(self):
-        if self.request_type == 'GET':
-            req = requests.get(url = self.url, headers = self.headers)
-            req.raise_for_status()
-            return req
-        elif self.request_type == 'POST':
-            req = requests.get(url = self.url, headers = self.headers, data = self.data)
-            req.raise_for_status()
-            return req
-        else:
-            raise Exception('Requersts other than GET or POST are not supported')
+    def _nonce(self):
+        return int(time.time()*1000)
+
+    def _sign(self, data):
+        j = json.dumps(data)
+        j = base64.standard_b64encode(j.encode('utf8'))
+        h = hmac.new(str.encode(self.api_secret), j, hashlib.sha384)
+        signature = h.hexdigest()
+        return {
+            "X-GEMINI-APIKEY": self.api_key,
+            "X-GEMINI-SIGNATURE": signature,
+            "X-GEMINI-PAYLOAD": j
+        }
 
 
-    def update_headers(self, new_headers):
-        self.headers.update(new_headers)
+    def getLastPrice(self, coin='ethusd'):
+        path = '/v1/pubticker/%s'%(coin)
+        response = requests.get(self.url + path)
+        return response.json()['last']
 
 
-req = GeminiRequest(base_url = 'https://api.gemini.com', path = 'v1/pubticker/ethusd')
-response = req.call()
-print(response.json())
+    def getPriceSpread(self, coin='ethusd'):
+        path = '/v1/pubticker/%s'%(coin)
+        response = requests.get(self.url + path)
+        return (response.json()['bid'], response.json()['ask'])
+
+
+    def checkBalances(self):
+        path = '/v1/balances'
+        data = {
+            "request": "/v1/balances",
+            "nonce": self._nonce()
+        }
+        headers = self._sign(data)
+        response = requests.post(self.url + path, headers = headers).json()
+        usd = [(x['available'], x['availableForWithdrawal'], x['amount']) for x in response if x['currency'] == 'USD'][0]
+        eth = [(x['available'], x['availableForWithdrawal'], x['amount']) for x in response if x['currency'] == 'ETH'][0]
+        btc = [(x['available'], x['availableForWithdrawal'], x['amount']) for x in response if x['currency'] == 'BTC'][0]
+        ret = {
+            'USD': {'Available': usd[0], 'Withdrawable': usd[1], 'Total': usd[2]},
+            'ETH': {'Available': eth[0], 'Withdrawable': eth[1], 'Total': eth[2]},
+            'BTC': {'Available': btc[0], 'Withdrawable': btc[1], 'Total': btc[2]}
+        }
+        return ret
